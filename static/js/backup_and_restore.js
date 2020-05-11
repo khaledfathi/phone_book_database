5b8cfd65f2144d4fdb2b22cc4f01f506afadc02c
Khaled Fathi <khaledfathi@protonmail.com>
2020-05-11 02:40:47 +0200

    Add files via upload

--- static/js/backup_and_restore.js
+++ static/js/backup_and_restore.js
@@ -1,1 +1,56 @@
-backup restore js
+//HTML ELEMENTS
+var backup_button =document.getElementById("backup_button"),
+    restore_button =document.getElementById("restore_button"),
+    backup_date_time = document.getElementById("backup_date_time"),
+    restore_date_time = document.getElementById("restore_date_time"),
+    history_table =document.getElementById("history_table"),
+    import_export_flag = document.getElementById("import_export_flag")
+    import_button = document.getElementById("import_button"),
+    export_button = document.getElementById("export_button"),
+    export_type_selected =document.getElementById("export_type_selected"),
+    export_file_type = document.getElementById("export_file_type");
+
+
+//api
+var api = {
+  case:""
+}
+
+backup_button.onclick = function (){
+  var req= new XMLHttpRequest();
+  req.onreadystatechange = function (){
+    if (this.readyState == 4 && this.status == 200){
+      data = JSON.parse(this.responseText)
+      backup_date_time.innerHTML = "Last Backup : " + data.last_info
+      history_table.innerHTML = data.table
+      alert("Database is Backedup")
+    }
+  }
+  req.open("POST","backup_and_restore", true)
+  req.setRequestHeader("Content-Type", "text/plain")
+  req.send("backup")
+}
+
+restore_button.onclick = function (){
+  var req= new XMLHttpRequest();
+  req.onreadystatechange = function (){
+    if (this.readyState == 4 && this.status == 200){
+      data = JSON.parse(this.responseText)
+      restore_date_time.innerHTML = "Last Restore : " + data.last_info
+      history_table.innerHTML = data.table
+      alert("Database is Restored")
+    }
+  }
+  req.open("POST","backup_and_restore", true)
+  req.setRequestHeader("Content-Type", "text/plain")
+  req.send("restore")
+}
+
+/*import export button actions*/
+import_button.onclick = function (){
+  import_export_flag.value = "import"
+}
+export_button.onclick = function (){
+  import_export_flag.value="export"
+  export_file_type.value = export_type_selected.options[export_type_selected.selectedIndex].innerText
+}
--- static/js/groups.js
+++ static/js/groups.js
@@ -4,20 +4,21 @@ var edit_group_select= document.getElementById("edit_group_select"),
     new_group_checkbox = document.getElementById("new_group_checkbox"),
     edit_save_button = document.getElementById("edit_save_button"),
     remove_move_to_new = document.getElementById("remove_move_to_new"),
+    new_group_text = document.getElementById("new_group_text"),
 //remove elements
     remove_group_select = document.getElementById("remove_group_select"),
     remove_radio = document.getElementsByName("remove_radio"),
     remove_group_move_to = document.getElementById("remove_group_move_to"),
-    remove_move_to_text = document.getElementById("remove_move_to_text"),
-    new_group_text = document.getElementById("new_group_text"),
+    remove_group_button = document.getElementById("remove_group_button"),
 //other
     add_api = document.getElementById("add_api")
-
 
 //api
 var api={
   case:"rename",
+  create_new:"0",
   selected_group:"default",
+  remove_option:"opt1"
 
 };
 
@@ -25,19 +26,17 @@ var api={
 //checkbox for create new group active text element and disable select element
 new_group_checkbox.onclick = function (){
   if (new_group_checkbox.checked){
-    api.case="create_new"
+    api.create_new="1"
     new_group_text.removeAttribute("disabled")
     rename_field.setAttribute("disabled", "")
     edit_group_select.setAttribute("disabled", "")
   }else{
-    api.case="rename"
+    api.create_new="0"
     new_group_text.setAttribute("disabled", "")
     rename_field.removeAttribute("disabled")
     edit_group_select.removeAttribute("disabled")
   }
 };
-
-
 
 //add api to form when submit button is clicked
 edit_save_button.onclick = function (){
@@ -47,7 +46,7 @@ edit_save_button.onclick = function (){
 //select_group will update summary table
 edit_group_select.onchange = function (){
   api["selected_group"] = edit_group_select.options[edit_group_select.selectedIndex].innerText
-  api["case"]="update_summary"
+  api["update_summary"]="1"
   req= new XMLHttpRequest();
   req.onreadystatechange = function (){
     if (this.readyState == 4 && this.status == 200){
@@ -67,20 +66,11 @@ edit_group_select.onchange = function (){
 
 
 
-
-
-
-
-
-/*#*****#* Summary*#*****#*/
-
-/*#*****#*END --Summary*#*****#*/
-
 /*#*****#* Remove Group *#*****#*/
-
 //actions when radio input selected
 for (var i=0;i<remove_radio.length;i++){
   remove_radio[i].addEventListener('click',function(event){
+    api.remove_option= event.target.value
     if (event.target.value=="opt2"){
       remove_group_move_to.removeAttribute("disabled")
     }else{
@@ -94,10 +84,47 @@ for (var i=0;i<remove_radio.length;i++){
   })
 }
 
+
+//remove gtoup button action
+remove_group_button.onclick= function (){
+  api.case="remove"
+  api.selected_group = remove_group_select.options[remove_group_select.selectedIndex].innerText
+  api.selected_group_to = remove_group_move_to.options[remove_group_move_to.selectedIndex].innerText
+  add_api.value = JSON.stringify(api)
+}
 /*#*****#*END --Remove Group *#*****#*/
 
+/*##Change flash message color##*/
+var flash_msg = document.getElementById("flash_msg"),
+  color_red_messages = [
+    "Nothing Changed : Empty Field",
+    "Error : You cant Edit 'default' Group",
+    "name 'default is not allowed'",
+    "Nothing Change : Duplicated Group Name",
+    "ERROR : Cant remove default group"
+    ],
+  color_green_messages =[
+    "Group Renamed",
+    "Group Removed",
+    "Group Removed and contacts moved to other group",
+    "New Group Created",
+    "Group and contacts are Removed",
+    ]
+
+var flash_msg = document.getElementById("flash_msg");
 
 
 
-/*****/
-setInterval(function(){console.log(JSON.stringify(api) , add_api.value)} , 500)
+for (var i=0;i<color_red_messages.length;i++){
+  if (flash_msg.innerText ==color_red_messages[i]){
+    flash_msg.style.color = "red"
+    break;
+  }
+}
+for (var i=0;i<color_green_messages.length;i++){
+  if (flash_msg.innerText ==color_green_messages[i]){
+    flash_msg.style.color = "green"
+    break;
+  }
+}
+/*##END -- Change flash message color##*/
